fab9f353c245463f0730473dd5b4531f55b16220
isabelaas <78918353+isabelaas@users.noreply.github.com>
2021-05-16 12:40:55 -0300

    Create math.c

---
+++ math.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+/* faca um algoritimo para resolver a equação z = X + Y-4 armazenando o resultado na variavel z.
+Considere que os valores iniciais de x e y sao, respectivamente, 10 e 5.
+*/
+const int X = 10, Y = 5;
+int Z = X + Y - 4;
+printf("o resultado e: %d", Z); 
+
+   return 0;
+}
